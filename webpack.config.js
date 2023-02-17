cfa427c5c097bef67db0bb97cea3151aef80fbf8
Pengu1Nus <2ez4artezzy@gmail.com>
2023-02-17 22:53:01 +0300

    feat: add webpack in project

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,67 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-module.exports = {
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    watch: !isProduction,
     entry: './src/index.js',
     output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true
-    }
-}
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'script.js',
+      clean: true,
+    },
+    devServer: {
+      open: true,
+      //host: '192.168.31.199',
+      static: {
+        directory: path.join(__dirname, './dist'),
+      },
+      compress: true,
+      port: 8080,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(css|scss|sass)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(png|svg|jpe?g|gif)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'images/[name][ext]',
+          },
+        },
+        {
+          test: /\.html$/,
+          use: ['html-loader'],
+        },
+      ],
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: 'style.css',
+      }),
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        filename: 'index.html',
+      }),
+    ],
+  };
+  return config;
+};
